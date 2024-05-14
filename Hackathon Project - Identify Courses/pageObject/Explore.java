085941ff7978ec833356f934bc3e9cae2c9dae02
nishchaygpt <nishchay.gupta.here@gmail.com>
2024-05-15 02:44:47 +0530

    Updated Explore.java

--- Hackathon Project - Identify Courses/pageObject/Explore.java
+++ Hackathon Project - Identify Courses/pageObject/Explore.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-//Author: Nishchay 
 
 public class Explore extends BasePage {
 
