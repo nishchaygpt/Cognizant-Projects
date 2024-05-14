90ca80944fc9397b227b244e38a1d1431cfd7c85
nishchaygpt <nishchay.gupta.here@gmail.com>
2024-05-15 02:45:16 +0530

    Updated ForBusiness.java

--- Hackathon Project - Identify Courses/pageObject/ForBusiness.java
+++ Hackathon Project - Identify Courses/pageObject/ForBusiness.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
-//Author: Anshu
 
 public class ForBusiness extends BasePage {
 	
